bff0b864364ed632547d3a74cef79bf42c88e139
TusharMazhar <tushar.talukdar@northsouth.edu>
2020-02-25 19:36:18 +0600

    lane , laneEnd, Stopsign sign detection completed and our car can follow this instructions

--- SELF_DRIVING_CAR/self_driving_car.cpp
+++ SELF_DRIVING_CAR/self_driving_car.cpp
@@ -3,20 +3,22 @@
 #include <iostream>
 #include <chrono> // chrono library used for calculate frame per second(FPS)
 #include <ctime> // this library is for calculate total time of execution
-
+#include <wiringPi.h>
 
 using namespace std;
 using namespace cv;
 using namespace raspicam;
 
-// these are Mat global variables
-Mat frame,Matrix,framePers,frameGray,frameThresh,frameEdge,frameFinal,ROILane,frameFinalDuplicate;
-int LeftLanePos,RightLanePos;
+// image peocessing----these are Mat global variables
+Mat frame,Matrix,framePers,frameGray,frameThresh,frameEdge,frameFinal,ROILane,ROILaneEnd,frameFinalDuplicate,frameFinalDuplicate1;
+int LeftLanePos,RightLanePos,laneCenter,frameCenter,Result,laneEnd;
 
 RaspiCam_Cv Camera; // Camera object created and it's global
+stringstream ss;
 vector<int> histrogramLane; // dynamic array created 
+vector<int> histrogramLaneEnd; // dynamic array created  for lANE END
 
-Point2f Source[] = {Point2f(42,145),Point2f(292,145),Point2f(12,195), Point2f(316,199)}; // 4 regions coordinate points, Stored in Source array with Point2f datatypes
+Point2f Source[] = {Point2f(31,145),Point2f(319,145),Point2f(6,195), Point2f(348,199)}; // 4 regions coordinate points, Stored in Source array with Point2f datatypes
 Point2f Destination[] = {Point2f(60,0),Point2f(300,0),Point2f(60,240), Point2f(300,240)};// create 4 perspective wrap points and stored in Destination array 
 // basic setup for our project
 void Setup ( int argc,char **argv, RaspiCam_Cv &Camera )
@@ -60,7 +62,7 @@ void Threshold()
 	add(frameThresh, frameEdge, frameFinal);
 	cvtColor(frameFinal, frameFinal, COLOR_GRAY2RGB);
 	cvtColor(frameFinal, frameFinalDuplicate, COLOR_RGB2BGR);   //used in histrogram function only
-	
+	cvtColor(frameFinal, frameFinalDuplicate1, COLOR_RGB2BGR);
 }
 
 void Histrogram()
@@ -74,6 +76,19 @@ void Histrogram()
 	divide(255, ROILane, ROILane);
 	histrogramLane.push_back((int)(sum(ROILane)[0])); 
     }
+    
+    histrogramLaneEnd.resize(360); // laneEnd detect
+    histrogramLaneEnd.clear(); 
+    
+    for(int i=0; i<360; i++)       //frame.size().width = 360
+    {
+	ROILaneEnd = frameFinalDuplicate1(Rect(i,0,1,240));
+	divide(255, ROILaneEnd, ROILaneEnd);
+	histrogramLaneEnd.push_back((int)(sum(ROILaneEnd)[0])); 
+    }
+    
+    laneEnd=sum(histrogramLaneEnd)[0];
+    cout<<" Lane End = "<<laneEnd<<endl; // laneEnd detect
 }
 
 
@@ -92,6 +107,16 @@ void LaneFinder()
     line(frameFinal, Point2f(RightLanePos, 0), Point2f(RightLanePos, 240), Scalar(0,255,0), 2); 
 }
 
+void LaneCenter()
+{
+    laneCenter = (RightLanePos-LeftLanePos)/2 +LeftLanePos;
+    frameCenter = 180;
+    
+    line(frameFinal, Point2f(laneCenter,0), Point2f(laneCenter,240), Scalar(0,255,0), 3); // draw line in console
+    line(frameFinal, Point2f(frameCenter,0), Point2f(frameCenter,240), Scalar(255,0,0), 3);// draw line in console
+
+    Result = laneCenter-frameCenter;
+}
 
 // Video capture  function
 void Capture()
@@ -103,7 +128,12 @@ void Capture()
 
 int main(int argc,char **argv)
 {
-    
+        wiringPiSetup();
+        pinMode(21,OUTPUT);
+        pinMode(22,OUTPUT);
+        pinMode(23,OUTPUT);
+        pinMode(24,OUTPUT);
+
    
     Setup(argc,argv,Camera); // argc and argc are the command line argument
     cout<<"Connecting to camera"<<endl;
@@ -124,7 +154,123 @@ int main(int argc,char **argv)
        Threshold();
        Histrogram();
        LaneFinder();
+       LaneCenter();
        
+    //uturn implemented 
+    
+    if (laneEnd > 15000)
+     {
+       	digitalWrite(21, 1);
+	digitalWrite(22, 1);    //decimal = 7
+	digitalWrite(23, 1);
+	digitalWrite(24, 0);
+	cout<<"Lane End"<<endl;
+     }
+    
+    //uturn close
+       
+    if(Result==0)
+    {
+         digitalWrite(21,0);
+         digitalWrite(22,0); // decimal =0
+         digitalWrite(23,0);
+         digitalWrite(24,0);
+         cout<<"Go Forward"<<endl;
+    }
+    else if(Result>0 && Result <10)
+    {
+         digitalWrite(21,1);
+         digitalWrite(22,0); // decimal =1
+         digitalWrite(23,0);
+         digitalWrite(24,0);
+         cout<<"Go Right1"<<endl;
+    }
+    else if(Result>=10 && Result <20)
+    {
+         digitalWrite(21,0);
+         digitalWrite(22,1); // decimal =2
+         digitalWrite(23,0);
+         digitalWrite(24,0);
+         cout<<"Go Right2"<<endl;
+    }
+    else if(Result>20)
+    {
+         digitalWrite(21,1);
+         digitalWrite(22,1); // decimal =3
+         digitalWrite(23,0);
+         digitalWrite(24,0);
+         cout<<"Go Right3"<<endl;
+    }
+
+
+
+    else if(Result<0 && Result >-10)
+    {
+         digitalWrite(21,0);
+         digitalWrite(22,0); // decimal =4
+         digitalWrite(23,1);
+         digitalWrite(24,0);
+         cout<<"Go Left1"<<endl;
+    }
+    else if(Result<=-10 && Result >-20)
+    {
+         digitalWrite(21,1);
+         digitalWrite(22,0); // decimal =5
+         digitalWrite(23,1);
+         digitalWrite(24,0);
+         cout<<"Go Left2"<<endl;
+    }
+    else if(Result<-20)
+    {
+         digitalWrite(21,0);
+         digitalWrite(22,1); // decimal =6
+         digitalWrite(23,1);
+         digitalWrite(24,0);
+         cout<<"Go Left3"<<endl;
+    }
+    
+    // laneEnd program
+    if(laneEnd>15000)
+    {
+       ss.str(" ");
+       ss.clear();
+       ss<<" Lane End ";
+       putText(frame, ss.str(), Point2f(1,50), 0,1, Scalar(0,255,255), 2);
+	
+    }
+    
+    else if(Result==0)
+    {
+       ss.str(" ");
+       ss.clear();
+       ss<<"Result = "<<Result<<"  (Move Forward )";
+       putText(frame, ss.str(), Point2f(1,50), 0,1, Scalar(0,0,255), 2);
+	
+    }
+    else if(Result> 0)
+    {
+       ss.str(" ");
+       ss.clear();
+       ss<<"Result = "<<Result<<"  (Move Right )";
+       putText(frame, ss.str(), Point2f(1,50), 0,1, Scalar(0,0,255), 2);
+	
+    }
+    
+    else if(Result< 0)
+    {
+       ss.str(" ");
+       ss.clear();
+       ss<<"Result = "<<Result<<"  (Move Left )";
+       putText(frame, ss.str(), Point2f(1,50), 0,1, Scalar(0,0,255), 2);
+	
+    }
+       
+       
+      
+     //LaneEnd program finished  
+     
+     
+     
        // original image
        namedWindow("Original RGB",WINDOW_KEEPRATIO);
        moveWindow("Original RGB",0,100); // window size
@@ -136,7 +282,7 @@ int main(int argc,char **argv)
        namedWindow("Perspective",WINDOW_KEEPRATIO);
        moveWindow("Perspective",640,100); // window size
        resizeWindow("Perspective",640,480); // resolation size
-       imshow("Perspective",framePers);// show the stored value in console
+       imshow("Perspective",framePers);//   (show the stored value in console
        
        // Grapy scale image
        namedWindow("Final",WINDOW_KEEPRATIO);
@@ -159,4 +305,3 @@ int main(int argc,char **argv)
     }
     return 0;
 }
- 
